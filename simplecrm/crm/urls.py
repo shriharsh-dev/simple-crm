d0d1121e6307cb4254e0385e106c7f4b66e05a48
Shriharsh <shriharsh.dev@gmail.com>
2025-04-01 05:00:53 +0000

    added urls file

---
+++ simplecrm/crm/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.DashboardView.as_view(), name='dashboard'),
+    path('customers/', views.CustomerListView.as_view(), name='customer-list'),
+    path('customers/add/', views.CustomerCreateView.as_view(), name='customer-create'),
+    path('customers/<int:pk>/', views.CustomerDetailView.as_view(), name='customer-detail'),
+    path('customers/<int:pk>/edit/', views.CustomerUpdateView.as_view(), name='customer-update'),
+    path('customers/<int:pk>/delete/', views.CustomerDeleteView.as_view(), name='customer-delete'),
+    path('customers/<int:pk>/add-interaction/', views.add_interaction, name='add-interaction'),
+]
